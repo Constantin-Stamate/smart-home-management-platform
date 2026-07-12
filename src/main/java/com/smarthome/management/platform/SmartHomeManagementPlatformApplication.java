085f8ea7527bd4d18ef1525165fafa801caa7415
Constantin_Stamate <constantinstamate.r@gmail.com>
2026-07-12 14:49:27 +0300

    build initial project commit

---
+++ src/main/java/com/smarthome/management/platform/SmartHomeManagementPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.smarthome.management.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeManagementPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHomeManagementPlatformApplication.class, args);
+	}
+}
---
+++ src/test/java/com/smarthome/management/platform/SmartHomeManagementPlatformApplicationTests.java
@@ -0,0 +1,12 @@
+package com.smarthome.management.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeManagementPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
